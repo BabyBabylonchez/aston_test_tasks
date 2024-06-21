6b9a67d72735de01227b5feefec538f902789ff8
Kirill Ovcharov <babybabylonmain@yandex.ru>
2024-06-21 10:25:19 +0300

    Task 2 done!

--- Task_2/src/Main.java
+++ Task_2/src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите строку a: ");
+        String a = scan.nextLine();
+        System.out.print("Введите строку b: ");
+        String b = scan.nextLine();
+
+        if(a.equals(b)){
+            System.out.println("Строки идентичны");
+        }
+        else{
+            System.out.println("Строки неидентичны");
+        }
+    }
 }
