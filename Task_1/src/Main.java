36923b3bcc685d187998c37d8fa849bd864215d7
Kirill Ovcharov <babybabylonmain@yandex.ru>
2024-06-21 10:08:43 +0300

    Task 1 done!

--- Task_1/src/Main.java
+++ Task_1/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число a: ");
+        Integer a = scan.nextInt();
+        System.out.print("Введите число b: ");
+        Integer b = scan.nextInt();
+        if (a > b) {
+            System.out.print("A > B");
+        } else if (a < b) {
+            System.out.print("A < B");
+        } else {
+            System.out.print("A = B");
+        }
+
+        System.out.printf("\nA + B = %d\nA - B = %d\nA / B = %f\nA * B = %d", a + b, a - b, a.floatValue() / b.floatValue(), a * b);
     }
 }
