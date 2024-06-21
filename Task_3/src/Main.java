f1e74e844935e3fd01685393ced9f5131c701d90
Kirill Ovcharov <babybabylonmain@yandex.ru>
2024-06-21 10:42:59 +0300

    Task 3 done!

--- Task_3/src/Main.java
+++ Task_3/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        int [] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int j : array1) {
+            if(j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+    }
 }
